b06971ac4799ae343ec2f4ec9478b5ba3fe67d09
sayid <herq12315@gmail.com>
2020-01-30 20:01:33 +0800

    add footer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   cd dist
+  [ -e CNAME ] && rm CNAME
   touch README.md
   echo -e "# 快速 setup node server\n\n初始化目录: \`npm init\`\n\n安装 http server: \`npm install http-server\`\n\n开启 http server: \`http-server [path] [options]\`\n\nServer 端添加 \`crontab\`:\n\n\`\`\`bash\ncrontab -e\n*/1 * * * * /bin/sh -c 'cd /var/www/event-output && git fetch --all && git reset --hard origin/master'\n\`\`\`" > README.md
 
@@ -15,7 +16,7 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
   exit 1
