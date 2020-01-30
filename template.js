8c6be1150509d6fff5fe94cb49719f8834c53c2f
Felix <fangli8080@gmail.com>
2020-01-30 15:06:45 +0800

    feat: add type

--- template.js
+++ template.js
@@ -5,8 +5,10 @@ const csvWriter = createCsvWriter({
   path: 'data.csv.example',
   header: [
     {id: 'id', title: 'id'},
+    {id: 'type', title: 'type'},
     {id: 'name', title: 'name'},
     {id: 'name_zh', title: 'name_zh'},
+    {id: 'name_zh_hk', title: 'name_zh_hk'},
     {id: 'confirmed', title: 'confirmed'},
     {id: 'death', title: 'death'},
     {id: 'note', title: 'note'}
@@ -21,7 +23,9 @@ const chinaData = data.objects.states_min.geometries.map(item => {
   return {
     id: item.properties.ne_id,
     name: item.properties.name,
+    type: 'china',
     name_zh: item.properties.name_zh,
+    name_zh_hk: item.properties.name_zh,
     confirmed: '',
     death: '',
     note: ''
@@ -32,7 +36,9 @@ const worldData = data.objects.countries_min.geometries.map(item => {
   return {
     id: item.properties.NE_ID,
     name: item.properties.NAME,
+    type: 'world',
     name_zh: item.properties.NAME_ZH,
+    name_zh_hk: item.properties.NAME_ZH,
     confirmed: '',
     death: '',
     note: ''
