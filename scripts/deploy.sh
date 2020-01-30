7e57850f57252084c89e17c45e2e8c6ede29b4c0
sayid <herq12315@gmail.com>
2020-01-30 17:00:39 +0800

    add travis cli

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
+  cd _site
+  rm CNAME
+  touch README.md
+  echo -e "# 快速 setup node server\n\n初始化目录: \`npm init\`\n\n安装 http server: \`npm install http-server\`\n\n开启 http server: \`http-server [path] [options]\`\n\nServer 端添加 \`crontab\`:\n\n\`\`\`bash\ncrontab -e\n*/1 * * * * /bin/sh -c 'cd /var/www/event-output && git fetch --all && git reset --hard origin/master'\n\`\`\`" > README.md
+
+  git init
+
+  git config user.name "sayidly"
+  git config user.email "herq12315@gmail.com"
+
+  git add .
+  git commit -m "Deploy"
+
+  # We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+else
+  echo 'Invalid branch. You can only deploy from gh-pages.'
+  exit 1
+fi
